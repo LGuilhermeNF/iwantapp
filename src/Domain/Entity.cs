53f43c5aba838a4ab8b7c980cfc0e7b1fb47d269
Guilherme Fernandes <guilherme.doze@hotmail.com>
2024-03-15 00:04:53 -0300

    first commit

---
+++ src/Domain/Entity.cs
@@ -0,0 +1,18 @@
+﻿using IWantApp.Domain.Products;
+
+namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreateBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string UpdateBy { get; set; }
+    public DateTime UpdatedOn { get; set; }
+}
