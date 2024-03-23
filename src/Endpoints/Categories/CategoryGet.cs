dad038790352dc0f0912d4abc5b32f4273818057
Guilherme Fernandes <guilherme.doze@hotmail.com>
2024-03-23 11:55:08 -0300

    Created get route

---
+++ src/Endpoints/Categories/CategoryGet.cs
@@ -0,0 +1,20 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IWantApp.Endpoints.Categories;
+
+public static class CategoryGet
+{
+    public static string Template => "/categories/{id}";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action([FromRoute] Guid id,ApplicationDbContext context)
+    {
+        var categories = context.Categories.Where(c => c.Id.Equals(id));
+        var response = categories.Select(c => new CategoryResponse { Id = c.Id, Name = c.Name, Active = c.Active});
+
+        return Results.Ok(response);
+    }
+}
